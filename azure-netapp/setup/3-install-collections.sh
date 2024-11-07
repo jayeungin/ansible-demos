91ecad2a5c33af5090e70d8c7d78d8d028a153ee
jayeungin <jasonyeung2694@gmail.com>
2024-11-07 17:33:23 -0500

    Add example ansible.cfg

--- azure-netapp/setup/3-install-collections.sh
+++ azure-netapp/setup/3-install-collections.sh
@@ -2,9 +2,7 @@
 
 ### assumes directory for container is mounted as /storage ###
 ### install python dependencies for Azure collection ###
-pip3.11 install -r /storage/azure-py-req.txt # insert file path
-pip3.11 install azure-mgmt-netapp
-pip3.11 install azure-mgmt
+pip3.11 install -r ../requirements.txt
 
 ### install Azure collection and NetApp.Azure collection ###
 ansible-galaxy collection install azure.azcollection
